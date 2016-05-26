e497a80722332ca585a29ec88424cd0d633191d3
punjabiInUSA <admissionsandme@gmail.com>
2016-05-26 17:37:22 -0500

    Updated 1. Single line editText fields logic, shall move to next editText 2. Updated app wide color changes to editText fields

--- app/src/main/java/com/learnandroid/appworld/UserActivity.java
+++ app/src/main/java/com/learnandroid/appworld/UserActivity.java
@@ -22,7 +22,6 @@ public class UserActivity extends AppCompatActivity {
 
     private Intent newIntent;
     AlertDialog.Builder mAlertDialog, mAlert;
-    Context mContext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,7 +42,7 @@ public class UserActivity extends AppCompatActivity {
         mButton = (Button) findViewById(R.id.btn_logout_user);
 
         if (mTextUser2 != null) {
-            mTextUser2.setText(mUserName2.toUpperCase());
+            mTextUser2.setText(mUserName2);
         }
     }
 
